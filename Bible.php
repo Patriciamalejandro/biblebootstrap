e0e5833084ec107fa202d1b294d38bf5a2892cb5
amylinebaccay <amylinebaccay@yahoo.com>
2015-10-24 12:19:38 +0800

    added

---
+++ Bible.php
@@ -0,0 +1,274 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap_v3/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap_v3/css/grid.css.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+
+
+			<div class="col-md-4">
+		<div role="tabpanel">
+
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#old" aria-controls="home" role="tab" data-toggle="tab">Old Testament</a></li>
+    <li role="presentation"><a href="#new" aria-controls="profile" role="tab" data-toggle="tab">New Testament</a></li>
+  </ul>
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane fade in active" id="old">
+    	<table class="table table-striped">
+    		<tr>
+    			<td>Genesis</td>
+    			<td><button type="button" class="btn btn-info">Read</button></td>
+    			<td><button type="button" class="btn btn-info">Info</button></td>
+    			<td><button type="button" class="btn btn-info">Watch</button></td>
+    		</tr>
+    		<tr>
+    			<td>Exodus</td>
+    			<td><button type="button" class="btn btn-info">Read</button></td>
+    			<td><button type="button" class="btn btn-info">Info</button></td>
+    			<td><button type="button" class="btn btn-info">Watch</button></td>
+    		</tr>
+    		<tr>
+    			<td>Leviticus</td>
+    			<td><button type="button" class="btn btn-info">Read</button></td>
+    			<td><button type="button" class="btn btn-info">Info</button></td>
+    			<td><button type="button" class="btn btn-info">Watch</button></td>
+    		</tr>
+            <tr>
+                <td>Joshua</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>Judges</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>Ruth</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>1 Samuel</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>2 Samuel</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>1 King</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>2 King</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>1 Cronicles</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>2 Cronicles</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+              <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+
+    	</table>
+    </div>
+    <div role="tabpanel" class="tab-pane fade" id="new">
+    	<table class="table table-striped">
+    		<tr>
+    			<td>Matthew</td>
+    			<td><button type="button" class="btn btn-info">Read</button></td>
+    			<td><button type="button" class="btn btn-info">Info</button></td>
+    			<td><button type="button" class="btn btn-info">Watch</button></td>
+    		</tr>
+    		<tr>
+    			<td>Mark</td>
+    			<td><button type="button" class="btn btn-info">Read</button></td>
+    			<td><button type="button" class="btn btn-info">Info</button></td>
+    			<td><button type="button" class="btn btn-info">Watch</button></td>
+    		</tr>
+    		<tr>
+    			<td>Luke</td>
+    			<td><button type="button" class="btn btn-info">Read</button></td>
+    			<td><button type="button" class="btn btn-info">Info</button></td>
+    			<td><button type="button" class="btn btn-info">Watch</button></td>
+    		</tr>
+            <tr>
+                <td>John</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>Acts</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>Romans</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td>1 Corinthians</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+             <tr>
+                <td>2 Corinthians</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr> <tr>
+                <td>Galatians</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+             <tr>
+                <td>Ephesians</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+             <tr>
+                <td>Philippians</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+             <tr>
+                <td>Colossians</td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type="button" class="btn btn-info">Read</button></td>
+                <td><button type="button" class="btn btn-info">Info</button></td>
+                <td><button type="button" class="btn btn-info">Watch</button></td>
+            </tr>
+
+    	</table>
+  </div>
+</div>
+			</div>
+			<div class="col-md-8">
+				right side
+			<div>
+		</div>
+	</div>
+	<script type="text/javascript" src = "jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="bootstrap_v3/js/bootstrap.js"></script>
+</body>
+</html>
